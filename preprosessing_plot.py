1b4ba3ffac866973eb1eb317ff80fafb91d8528e
Lizy321 <32933043+Lizy321@users.noreply.github.com>
2018-05-06 14:44:40 +0200

    Add files via upload

--- preprosessing_plot.py
+++ preprosessing_plot.py
@@ -1,18 +1,28 @@
 import datetime
 import time
+import random
+
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-from sklearn import neighbors
+
+from sklearn import neighbors,metrics
 from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import confusion_matrix
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_curve, auc
+from sklearn.neural_network import MLPClassifier
+
 from operator import itemgetter
 from itertools import groupby
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
-
+from imblearn.over_sampling import SMOTE
+from collections import Counter
 
 def string_to_timestamp(date_string):#convert time string to float value
     time_stamp = time.strptime(date_string, '%Y-%m-%d %H:%M:%S')
@@ -80,10 +90,9 @@ def aggregate_mean(before_aggregate):
         #print before_aggregate[0]
     return after_aggregate
 
-src = '/Users/lizy/my_doc/Q4/cyber_data_analysis/CS4035 Cyber Data Analytics (201718 Q4) - 4282018 - 441 PM/data_for_student_case.csv'
+src = '/Users/lizy/my_doc/Q4/cyber_data_analysis/CS4035_Cyber_Data_Analytics/data/data_for_student_case.csv'
 ah = open(src, 'r')
-x = []#contains features
-y = []#contains labels
+
 data = []
 color = []
 (issuercountry_set, txvariantcode_set, currencycode_set, shoppercountry_set, interaction_set,
@@ -180,88 +189,6 @@ for item in client_aggregate:
     plt.savefig('Client Aggregating.png')
 
 
-# In[6]:
-
-for item in data:#split data into x,y
-    x.append(item[0:-2])
-    y.append(item[-2])
-'''map number to each categorial feature'''
-for item in list(issuercountry_set):
-    issuercountry_dict[item] = list(issuercountry_set).index(item)
-for item in list(txvariantcode_set):
-    txvariantcode_dict[item] = list(txvariantcode_set).index(item)
-for item in list(currencycode_set):
-    currencycode_dict[item] = list(currencycode_set).index(item)
-for item in list(shoppercountry_set):
-    shoppercountry_dict[item] = list(shoppercountry_set).index(item)
-for item in list(interaction_set):
-    interaction_dict[item] = list(interaction_set).index(item)
-for item in list(verification_set):
-    verification_dict[item] = list(verification_set).index(item)
-for item in list(accountcode_set):
-    accountcode_dict[item] = list(accountcode_set).index(item)
-print len(list(card_id_set))
-#for item in list(card_id_set):
-#    card_id_dict[item] = list(card_id_set).index(item)
-'''modify categorial feature to number in data set'''
-for item in x:
-    item[0] = issuercountry_dict[item[0]]
-    item[1] = txvariantcode_dict[item[1]]
-    item[4] = currencycode_dict[item[4]]
-    item[5] = shoppercountry_dict[item[5]]
-    item[6] = interaction_dict[item[6]]
-    item[7] = verification_dict[item[7]]
-    item[10] = accountcode_dict[item[10]]
-
-#x_mean = []
-#x_mean = aggregate_mean(x);
-x_mean = x;
-des = '/Users/lizy/my_doc/Q4/cyber_data_analysis/CS4035 Cyber Data Analytics (201718 Q4) - 4282018 - 441 PM/original_data.csv'
-des1 = '/Users/lizy/my_doc/Q4/cyber_data_analysis/CS4035 Cyber Data Analytics (201718 Q4) - 4282018 - 441 PM/aggregate_data.csv'
-ch_dfa = open(des,'w')
-#ch_dfa.write('txid,bookingdate,issuercountrycode,txvariantcode,bin,amount,'+
-#             'currencycode,shoppercountrycode,shopperinteraction,simple_journal,'+
- #            'cardverificationcodesupplied,cvcresponsecode,creationdate,accountcode,mail_id,ip_id,card_id')
-#ch_dfa.write('\n')
-sentence = []
-for i in range(len(x_mean)):
-    for j in range(len(x_mean[i])):
-        sentence.append(str(x_mean[i][j]))
-    sentence.append(str(y[i]))
-    ch_dfa.write(' '.join(sentence))
-    ch_dfa.write('\n')
-    sentence=[]
-    ch_dfa.flush()
-TP, FP, FN, TN = 0, 0, 0, 0
-x_array = np.array(x)
-y_array = np.array(y)
-usx = x_array
-usy = y_array
-x_train, x_test, y_train, y_test = train_test_split(usx, usy, test_size = 0.2)#test_size: proportion of train/test data
-clf = neighbors.KNeighborsClassifier(algorithm = 'kd_tree')
-clf.fit(x_train, y_train)
-y_predict = clf.predict(x_test)
-for i in xrange(len(y_predict)):
-    if y_test[i]==1 and y_predict[i]==1:
-        TP += 1
-    if y_test[i]==0 and y_predict[i]==1:
-        FP += 1
-    if y_test[i]==1 and y_predict[i]==0:
-        FN += 1
-    if y_test[i]==0 and y_predict[i]==0:
-        TN += 1
-print 'TP: '+ str(TP)
-print 'FP: '+ str(FP)
-print 'FN: '+ str(FN)
-print 'TN: '+ str(TN)
-#print confusion_matrix(y_test, answear) watch out the element in confusion matrix
-precision, recall, thresholds = precision_recall_curve(y_test, y_predict)
-predict_proba = clf.predict_proba(x_test)#the probability of each smple labelled to positive or negative
-
-print 'predict probability: ' + str(predict_proba)
-
-#data_all = pd.read_csv('/Users/lizy/my_doc/Q4/cyber_data_analysis/CS4035 Cyber Data Analytics (201718 Q4) - 4282018 - 441 PM/data_for_student_case.csv')
-
 
 # retrieve the fraud transactions
 fraud = data_all.loc[data_all['label']==1]
@@ -309,3 +236,281 @@ for item in fraud_client_aggregate:
     plt.ylabel('Number of Transactions')
     plt.axis('tight')
     plt.savefig('/Users/lizy/my_doc/Q4/cyber_data_analysis/CS4035 Cyber Data Analytics (201718 Q4) - 4282018 - 441 PM/Fraud Client Aggregating.png')
+
+
+# In[6]:
+x = []#contains features
+y = []#contains labels
+for item in data:#split data into x,y
+    x.append(item[0:-2])
+    y.append(item[-2])
+
+X = x[:]
+'''map number to each categorial feature'''
+for item in list(issuercountry_set):
+    issuercountry_dict[item] = list(issuercountry_set).index(item)
+for item in list(txvariantcode_set):
+    txvariantcode_dict[item] = list(txvariantcode_set).index(item)
+for item in list(currencycode_set):
+    currencycode_dict[item] = list(currencycode_set).index(item)
+for item in list(shoppercountry_set):
+    shoppercountry_dict[item] = list(shoppercountry_set).index(item)
+for item in list(interaction_set):
+    interaction_dict[item] = list(interaction_set).index(item)
+for item in list(verification_set):
+    verification_dict[item] = list(verification_set).index(item)
+for item in list(accountcode_set):
+    accountcode_dict[item] = list(accountcode_set).index(item)
+print len(list(card_id_set))
+#for item in list(card_id_set):
+#    card_id_dict[item] = list(card_id_set).index(item)
+'''modify categorial feature to number in data set'''
+for item in x:
+    item[0] = issuercountry_dict[item[0]]
+    item[1] = txvariantcode_dict[item[1]]
+    item[4] = currencycode_dict[item[4]]
+    item[5] = shoppercountry_dict[item[5]]
+    item[6] = interaction_dict[item[6]]
+    item[7] = verification_dict[item[7]]
+    item[10] = accountcode_dict[item[10]]
+
+#x_mean = []
+#x_mean = aggregate_mean(x);
+
+# x_mean = x[:];
+# des = '/Users/lizy/my_doc/Q4/cyber_data_analysis/CS4035_Cyber_Data_Analytics/data/original_data.csv'
+# des1 = '/Users/lizy/my_doc/Q4/cyber_data_analysis/CS4035_Cyber_Data_Analytics/data/aggregate_data.csv'
+# ch_dfa = open(des,'w')
+
+#ch_dfa.write('txid,bookingdate,issuercountrycode,txvariantcode,bin,amount,'+
+#             'currencycode,shoppercountrycode,shopperinteraction,simple_journal,'+
+ #            'cardverificationcodesupplied,cvcresponsecode,creationdate,accountcode,mail_id,ip_id,card_id')
+#ch_dfa.write('\n')
+
+# sentence = []
+# for i in range(len(x_mean)):
+#     for j in range(len(x_mean[i])):
+#         sentence.append(str(x_mean[i][j]))
+#     sentence.append(str(y[i]))
+#     ch_dfa.write(' '.join(sentence))
+#     ch_dfa.write('\n')
+#     sentence=[]
+#     ch_dfa.flush()
+#
+
+
+TP, FP, FN, TN = 0, 0, 0, 0
+x_array = np.array(x)
+y_array = np.array(y)
+
+x_train, x_test, y_train, y_test = train_test_split(x_array, y_array, test_size = 0.2)#test_size: proportion of train/test data
+
+# choose the same number of fraud case from training set
+fraud_train_y = y_train[y_train==1]
+nonfraud_train_y = y_train[y_train==0]
+fraud_train_x = x_train[y_train==1]
+nonfraud_train_x = x_train[y_train==0]
+
+num_fraud = len(fraud_train_y)
+num_non_fraud = len(nonfraud_train_y)
+
+num_test_fraud = len(y_test[y_test==1])
+
+def ran_sample(ds_x,ds_y,total,size):
+
+    idx = list(np.random.randint(total, size=size))
+    sample_x = ds_x[idx]
+    sample_y = ds_y[idx]
+
+    return sample_x,sample_y
+
+'''fraud data'''
+fraud_train_x, fraud_train_y = ran_sample(fraud_train_x,fraud_train_y,num_test_fraud,num_test_fraud)
+
+'''DS 1: proportion 15%'''
+#number of non-fraud would be 351
+num_non_fraud_ds1 = num_test_fraud/15*85
+nonfraud1_train_x, nonfraud1_train_y = ran_sample(nonfraud_train_x,nonfraud_train_y,num_non_fraud,num_non_fraud_ds1)
+
+X_train_ds1 = np.vstack((fraud_train_x,nonfraud1_train_x))
+Y_train_ds1 = np.append(fraud_train_y,nonfraud1_train_y)
+
+'''DS 2: proportion 10%'''
+#number of non-fraud would be 351
+num_non_fraud_ds2 = num_test_fraud/10*90
+nonfraud2_train_x, nonfraud2_train_y = ran_sample(nonfraud_train_x,nonfraud_train_y,num_non_fraud,num_non_fraud_ds2)
+
+X_train_ds2 = np.vstack((fraud_train_x,nonfraud2_train_x))
+Y_train_ds2 = np.append(fraud_train_y,nonfraud2_train_y)
+
+'''DS 3: proportion 5%'''
+#number of non-fraud would be 351
+num_non_fraud_ds3 = num_test_fraud/5*95
+nonfraud3_train_x, nonfraud3_train_y = ran_sample(nonfraud_train_x,nonfraud_train_y,num_non_fraud,num_non_fraud_ds3)
+
+X_train_ds3 = np.vstack((fraud_train_x,nonfraud3_train_x))
+Y_train_ds3 = np.append(fraud_train_y,nonfraud3_train_y)
+
+'''DS 4: proportion 2%'''
+#number of non-fraud would be 351
+num_non_fraud_ds4 = num_test_fraud/2*98
+nonfraud4_train_x, nonfraud4_train_y = ran_sample(nonfraud_train_x,nonfraud_train_y,num_non_fraud,num_non_fraud_ds4)
+
+X_train_ds4 = np.vstack((fraud_train_x,nonfraud4_train_x))
+Y_train_ds4 = np.append(fraud_train_y,nonfraud4_train_y)
+
+
+def plot_roc(predict_proba,y_test,y_predict,idx):
+    TP, FP, FN, TN = 0, 0, 0, 0
+    for i in xrange(len(y_predict)):
+        if y_test[i] == 1 and y_predict[i] == 1:
+            TP += 1
+        if y_test[i] == 0 and y_predict[i] == 1:
+            FP += 1
+        if y_test[i] == 1 and y_predict[i] == 0:
+            FN += 1
+        if y_test[i] == 0 and y_predict[i] == 0:
+            TN += 1
+    print 'TP: ' + str(TP)
+    print 'FP: ' + str(FP)
+    print 'FN: ' + str(FN)
+    print 'TN: ' + str(TN)
+    print confusion_matrix(y_test, y_predict)  # watch out the element in confusion matrix
+    precision, recall, thresholds = precision_recall_curve(y_test, y_predict)
+    print('precision: ' + str(precision))
+    print('recall: ' + str(recall))
+
+    # y_test = y_test.reshape((47340,1))
+    auc = metrics.roc_auc_score(y_test, predict_proba)
+    fpr, tpr, _ = metrics.roc_curve(y_test, predict_proba)
+    #plt.figure()
+    plt.title('Receiver Operating Characteristic')
+    plt.plot(fpr, tpr, label="data" + idx + ", auc=" + str(auc))
+    plt.legend(loc='lower right')
+    plt.plot([0, 1], [0, 1], 'r--')
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.show()
+    plt.savefig("/Users/lizy/my_doc/Q4/cyber_data_analysis/Cyber-Data-Analysis/plot/DS-"+idx+".png")
+
+
+'''SVM'''
+def svm_train(x_train,y_train,x_test,y_test):
+    clf = svm.SVC()
+    clf.fit(x_train, y_train)
+    # clf = neighbors.KNeighborsClassifier(algorithm = 'kd_tree')
+    # clf.fit(x_train, y_train)
+    y_predict = clf.predict(x_test)
+    print("svm accuracy: ")
+    print accuracy_score(y_test, y_predict)
+    idx = 'none'
+    predict_proba = clf.predict_proba(x_test)  # the probability of each smple labelled to positive or negative
+    plot_roc(predict_proba,y_test,y_predict,idx)
+
+    print('\n')
+
+'''ds1 result'''
+svm_train(X_train_ds1,Y_train_ds1,x_test,y_test)
+svm_train(X_train_ds2,Y_train_ds2,x_test,y_test)
+svm_train(X_train_ds3,Y_train_ds3,x_test,y_test)
+svm_train(X_train_ds4,Y_train_ds4,x_test,y_test)
+
+'''Random Forest'''
+def rf_train(x_train,y_train,x_test,y_test,idx):
+    clf_r = RandomForestClassifier(max_depth=2, random_state=0)
+    clf_r.fit(x_train, y_train)
+    y_predict = clf_r.predict(x_test)
+    print("random forest accuracy: ")
+    print accuracy_score(y_test, y_predict)
+    predict_proba = clf_r.predict_proba(x_test)[:, 1]  # the probability of each smple labelled to positive or negative
+    plot_roc(predict_proba,y_test,y_predict,idx)
+    print('\n')
+
+'''ds1 result'''
+print("DS1:")
+rf_train(X_train_ds1,Y_train_ds1,x_test,y_test,'1')
+print("DS2:")
+rf_train(X_train_ds2,Y_train_ds2,x_test,y_test,'2')
+print("DS3:")
+rf_train(X_train_ds3,Y_train_ds3,x_test,y_test,'3')
+print("DS4:")
+rf_train(X_train_ds4,Y_train_ds4,x_test,y_test,'4')
+
+
+'''Neural Network '''
+def nn_train(x_train,y_train,x_test,y_test,idx):
+    x_train = x_train.astype('float64')
+    y_train = y_train.astype('float64')
+    clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5,5,5), random_state=1)
+    clf.fit(x_train, y_train)
+    x_test = x_test.astype('float64')
+    y_test = y_test.astype('float64')
+    y_predict = clf.predict(x_test)
+    print("random forest accuracy: ")
+    print accuracy_score(y_test, y_predict)
+    predict_proba = clf.predict_proba(x_test)[:, 1]  # the probability of each smple labelled to positive or negative
+    plot_roc(predict_proba, y_test, y_predict, idx)
+    print('\n')
+
+print("DS1:")
+nn_train(X_train_ds1,Y_train_ds1,x_test,y_test,'1(NN)')
+print("DS2:")
+nn_train(X_train_ds2,Y_train_ds2,x_test,y_test,'2(NN)')
+print("DS3:")
+nn_train(X_train_ds3,Y_train_ds3,x_test,y_test,'3(NN)')
+print("DS4:")
+nn_train(X_train_ds4,Y_train_ds4,x_test,y_test,'4(NN)')
+
+
+#data_all = pd.read_csv('/Users/lizy/my_doc/Q4/cyber_data_analysis/CS4035 Cyber Data Analytics (201718 Q4) - 4282018 - 441 PM/data_for_student_case.csv')
+
+#data_all.to_csv('/Users/lizy/my_doc/Q4/cyber_data_analysis/CS4035_Cyber_Data_Analytics/data/data.csv')
+
+
+
+'''=========== RESAMPLE SMOTE =============='''
+def sm_sample(x_train,y_train,x_test,y_test,idx):
+    sm = SMOTE()
+    # x_array = np.array(x)
+    # y_array = np.array(y)
+    usx = x_train.astype('float64')
+    usy = y_train.astype('float64')
+
+    X_res, y_res = sm.fit_sample(usx, usy)
+    print(format(Counter(y_train)))
+    print('Resampled dataset shape {}'.format(Counter(y_res)))
+
+    print("Random Forest: ")
+    rf_train(X_res,y_res,x_test,y_test,idx)
+    #nn_train(X_res,y_res,x_test,y_test,idx)
+    # print("SVM:")
+    # svm_train(X_res, y_res, x_test, y_test)
+plt.figure()
+sm_sample(X_train_ds1,Y_train_ds1,x_test,y_test,'SM-1')
+sm_sample(X_train_ds2,Y_train_ds2,x_test,y_test,'SM-2')
+sm_sample(X_train_ds3,Y_train_ds3,x_test,y_test,'SM-3')
+sm_sample(X_train_ds4,Y_train_ds4,x_test,y_test,'SM-4')
+
+plt.figure()
+rf_train(X_train_ds1,Y_train_ds1,x_test,y_test,'1(ORI)')
+sm_sample(X_train_ds1,Y_train_ds1,x_test,y_test,'1(SMOTE)')
+
+plt.figure()
+rf_train(X_train_ds2,Y_train_ds2,x_test,y_test,'2(ORI)')
+sm_sample(X_train_ds2,Y_train_ds2,x_test,y_test,'2(SMOTE)')
+
+plt.figure()
+rf_train(X_train_ds3,Y_train_ds3,x_test,y_test,'3(ORI)')
+sm_sample(X_train_ds3,Y_train_ds3,x_test,y_test,'3(SMOTE)')
+
+plt.figure()
+rf_train(X_train_ds3,Y_train_ds3,x_test,y_test,'3(ORI)')
+sm_sample(X_train_ds3,Y_train_ds3,x_test,y_test,'3(SMOTE)')
+
+plt.figure()
+nn_train(X_train_ds1,Y_train_ds1,x_test,y_test,'1(nn-ORI)')
+sm_sample(X_train_ds1,Y_train_ds1,x_test,y_test,'1(nn-SMOTE)')
+
+# clf = neighbors.KNeighborsClassifier(algorithm = 'kd_tree')
